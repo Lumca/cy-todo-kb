dab2efdc97951bd2c4b43b1246d0913b4f7a520d
Lumír Verner <verner@lumir.me>
2020-11-22 17:41:44 +0100

    Added typescript interface and gitignore for idea

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,5 +1,103 @@
 /// <reference types="cypress" />
 
 declare namespace Cypress {
+    interface Chainable<Subject> {
+        /**
+         * Creates one Todo using UI
+         * @example
+         * cy.createTodo('new todo')
+         */
+        createTodo(createTodo: string): Chainable<any>
 
+        /**
+         * Selects specific Todo item via UI
+         * @example
+         * cy.clickSelectTodo('todo')
+         */
+        clickSelectTodo(selectTodo: string): Chainable<any>
+
+        /**
+         * Edits todo via UI
+         * @example
+         * cy.editTodo('current todo', 'edited todo')
+         */
+        editTodo(currentTodo: string, editTodo: string): Chainable<any>
+
+        /**
+         * Deletes todo via UI
+         * @example
+         * cy.deleteTodo('todo')
+         */
+        deleteTodo(deleteTodo: string): Chainable<any>
+
+        /**
+         * Click All button todo via UI
+         * @example
+         * cy.clickAllButton()
+         */
+        clickAllButton(): Chainable<any>
+
+        /**
+         * Click Active button todo via UI
+         * @example
+         * cy.clickActiveButton()
+         */
+        clickActiveButton(): Chainable<any>
+
+        /**
+         * Click Completed button todo via UI
+         * @example
+         * cy.clickCompletedButton()
+         */
+        clickCompletedButton(): Chainable<any>
+
+        /**
+         * Click Clear completed button todo via UI
+         * @example
+         * cy.clickClearCompletedButton()
+         */
+        clickClearCompletedButton(): Chainable<any>
+
+        /**
+         * checks todo label exists
+         * @example
+         * cy.checkTodoLabelExists()
+         */
+        checkTodoLabelExists(): Chainable<any>
+
+        /**
+         * checks new todo input exists
+         * @example
+         * cy.checkNewTodoInputExists()
+         */
+        checkNewTodoInputExists(): Chainable<any>
+
+        /**
+         * checks footer label exists
+         * @example
+         * cy.checkFooterLabelExists()
+         */
+        checkFooterLabelExists(): Chainable<any>
+
+        /**
+         * checks Filters buttons exists
+         * @example
+         * cy.checkFooterLabelExists()
+         */
+        checkFilterLabelExists(): Chainable<any>
+
+        /**
+         * check Clear Completed Button Exists
+         * @example
+         * cy.checkClearCompletedButtonExists()
+         */
+        checkClearCompletedButtonExists(): Chainable<any>
+
+        /**
+         * Check todo counter exists with right count
+         * @example
+         * cy.checkTodoCounterLabelCount()
+         */
+        checkTodoCounterLabelCount(count: string): Chainable<any>
+    }
 }
